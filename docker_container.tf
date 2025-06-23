05191f088d9921b52b4a9d28245825aab5cadaa4
bagroo <florimond2002@gmail.com>
2025-06-23 13:56:09 +0200

    refactor: divide main.tf into files by resources

---
+++ docker_container.tf
@@ -0,0 +1,35 @@
+resource "docker_container" "nginx" {
+  name  = var.containers["nginx-terraform"].container_name
+  image = docker_image.nginx.image_id
+  ports {
+    internal = var.containers["nginx-terraform"].ports["internal"]
+    external = var.containers["nginx-terraform"].ports["external"]
+  }
+
+  networks_advanced {
+    name = docker_network.private_network.name
+    # aliases = "nginx"
+  }
+}
+
+resource "docker_container" "client" {
+
+  count = var.number_of_clients
+
+  name  = "${var.containers["client"].container_name}-${count.index + 1}"
+  image = docker_image.client.name
+  ports {
+    internal = var.containers["client"].ports["internal"]
+    external = var.containers["client"].ports["external"]
+  }
+
+  depends_on = [docker_container.nginx]
+
+  command = [
+    "/bin/sh", "-c",
+    "curl -s http://nginx-terraform:80 && echo 'Nginx is reachable' || echo 'Nginx is not reachable'; sleep 15"
+  ]
+  networks_advanced {
+    name = docker_network.private_network.name
+  }
+}
