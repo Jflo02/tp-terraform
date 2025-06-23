3ad7ef81c37d30132096ab7f9ae589801cf5ce7b
bagroo <florimond2002@gmail.com>
2025-06-23 09:55:25 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "test_file" {
+  content  = "Hello depuis Terraform !​🚀​"
+  filename = "${path.module}/hello.txt"
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Commande exécutée !'"
+  }
+}
