bd55d377bff55e50a8641af2f6a8b6d430405411
bagroo <florimond2002@gmail.com>
2025-06-24 09:38:50 +0200

    fmt

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 
 variable "ec2" {
-    type = object({
-      instance_type = string
-      name           = string
-    })
-    default = {
-      instance_type = "t2.micro"
-      name           = "nginx-terraform-instance"
-    }
+  type = object({
+    instance_type = string
+    name          = string
+  })
+  default = {
+    instance_type = "t2.micro"
+    name          = "nginx-terraform-instance"
+  }
 }
 
-variable sg_port_allow_http {
+variable "sg_port_allow_http" {
   type    = number
   default = 80
 }
-variable sg_port_allow_ssh {
+variable "sg_port_allow_ssh" {
   type    = number
   default = 22
 }
-variable sg_port_allow_all_outbound {
+variable "sg_port_allow_all_outbound" {
   type    = number
   default = 0
 }
